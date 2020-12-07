4adf54190de5abcb6e268473bbb0bca5e9eb4d37
Benjamin Ng <nicky51707@gmail.com>
2020-12-07 17:08:57 +0800

    2039

--- week04/scripts.js
+++ week04/scripts.js
@@ -4,4 +4,16 @@ function transformUpperCase(){
 }
 
 let elUsername = document.getElementById("namaPengguna");
-elUsername.addEventListener("keyup", transformUpperCase);
+elUsername.addEventListener("keyup", transformUpperCase); // New/Recommended Way
+
+function checkPasswordLength(){
+    let password = document.getElementById("kataLaluan").value;
+    if(password.length < 5) {
+        alert("Password must be more than 5 characters");
+    } else {
+        alert("Password accepted!")
+    }
+}
+
+let elPassword = document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength; // Old/Classic Way
