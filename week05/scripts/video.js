a84ef11a0cff6075cda00942bca493470c2959b7
Benjamin Ng <nicky51707@gmail.com>
2020-12-08 21:13:19 +0800

    2113

--- week05/scripts/video.js
+++ week05/scripts/video.js
@@ -18,4 +18,15 @@ window.onload = function(){
     freezeBtn.addEventListener("mouseup",function(){
         video.play();
     });
+
+    let seekBar = document.getElementById("seekBar");
+    seekBar.addEventListener("change",function(){
+        let time = video.duration * (seekBar.value / 100);
+        video.currentTime = time;
+    });
+    
+    video.addEventListener("timeupdate",function(){
+        let value = (100 / video.duration) * video.curentTime;
+        seekBar. = value;
+    });
 };
