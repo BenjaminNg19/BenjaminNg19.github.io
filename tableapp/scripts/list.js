a6fb6524818369f576e465cccbfc3af6a9c242b4
Benjamin Ng <nicky51707@gmail.com>
2020-12-22 20:49:57 +0800

    2049

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -35,3 +35,9 @@ function GetBookings(){
             }
         });
 }
+
+window.addEventListener("load", function () {
+    document.getElementById("refreshList").addEventListener("click",function(){
+        GetBookings();
+    });
+})
