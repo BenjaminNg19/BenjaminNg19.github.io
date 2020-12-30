9e2d4e471d3eb7125a857077b3c58891ccef2e5b
Benjamin Ng <nicky51707@gmail.com>
2020-12-30 12:16:07 +0800

    1216

--- dentalapp/scripts/book.js
+++ dentalapp/scripts/book.js
@@ -25,8 +25,21 @@ function BookNow(userName, userEmail, userPhone, userRemarks, userDate, userTime
 
 window.addEventListener("load", function(){
         let today = new Date().toISOString().slice(0, 10);
-        console.log(today);
         document.getElementById("userDate").setAttribute("min", today);
+
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form)
+        {
+          form.addEventListener('submit', function(event) 
+          {
+            if (form.checkValidity() === false) 
+            {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
 
     document.getElementById("bookNow").addEventListener("click",function(){
         let userName = document.getElementById("userName").value;
