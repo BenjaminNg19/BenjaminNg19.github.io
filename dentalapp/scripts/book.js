ac966d05b838e52870b132294203e3a04c6373c9
Benjamin Ng <nicky51707@gmail.com>
2020-12-30 16:21:30 +0800

    1621

--- dentalapp/scripts/book.js
+++ dentalapp/scripts/book.js
@@ -25,16 +25,14 @@ function BookNow(userName, userEmail, userPhone, userRemarks, userDate, userTime
 
 window.addEventListener("load", function(){
     let today = new Date().toISOString().slice(0, 10);
-    let validated = true;
     document.getElementById("userDate").setAttribute("min", today);
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName("needs-validation");
+    var forms = document.getElementsByClassName("needs-validation").validation;
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener("submit", function(event) {
         if (form.checkValidity() === false) {
-          validated = false;
           event.preventDefault();
           event.stopPropagation();
         }
@@ -42,7 +40,7 @@ window.addEventListener("load", function(){
       }, false);
     });
 
-    if(validated === true)
+    if(forms === true)
     {
       document.getElementById("bookNow").addEventListener("click",function(){
           let userName = document.getElementById("userName").value;
